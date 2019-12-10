0ab63774c714cb7c71b7e998ac8692a757c4bac5
ravindra639 <42795993+ravindra639@users.noreply.github.com>
2019-12-10 16:56:59 +0530

    Add files via upload

---
+++ Firstservice/service.bat
@@ -0,0 +1,3 @@
+sc delete WindowsDemoService
+C:\\WINDOWS\\Microsoft.NET\\Framework64\\v4.0.30319\\InstallUtil.exe "%WORKSPACE%\\Firstservice\\Firstservice\\bin\\Release\\Firstservice.exe"
+Net Start WindowsDemoService
